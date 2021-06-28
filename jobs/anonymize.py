b117bb40a4728ca2adbc68f9e60ba5829f75f443
Jake <a.b@c.com>
2021-06-28 12:44:26 +1000

    Added comments

--- jobs/anonymize.py
+++ jobs/anonymize.py
@@ -1,9 +1,9 @@
 from pyspark.sql.functions import soundex, trim, when
 
 
+# This function uses soundex to anonymize the first_name and last_name columns.
+# The address column is replaced with xxxxx if it has some value otherwise it's left as such
 def anonymize_columns(input_df):
-    # This function uses soundex to anonymize the first_name and last_name columns.
-    # The address column is replaced with xxxxx if it has some value otherwise it's left as such
     return input_df.select(soundex(trim(input_df["first_name"])).alias("first_name"),
                            soundex(trim(input_df["last_name"])).alias("last_name"),
                            when(input_df["address"].isNull(), input_df["address"]).otherwise('xxxxx').alias("address"),
@@ -11,10 +11,11 @@ def anonymize_columns(input_df):
                            )
 
 
+# This function is used to save the output to the location given in the config
 def save_output(config, output_df):
     output_df.write.option("header", True).mode("overwrite").csv(f"{config.get('output_data_path')}/people")
 
-
+# This function is used to load the data and create a data frame from the input file
 def load_data(spark, config):
     return (
         spark.read.format("csv").option("header", True).load(f"{config.get('source_data_path')}/people.csv")
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import json
-
 from pyspark.sql import SparkSession
 from jobs import anonymize
 
@@ -7,9 +6,7 @@ from jobs import anonymize
 def main():
     with open("config.json", "r") as config_file:
         config = json.load(config_file)
-
     spark = SparkSession.builder.appName(config.get("app_name")).getOrCreate()
-
     anonymize.run_job(spark, config)
 
 
